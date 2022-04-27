cc4e27c29e678810235d3c341329dcad8da5b742
NavidKJalali <navid.jalali@prezi.com>
2022-04-27 22:49:30 +0200

    🔧 go back to dotty

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -231,7 +231,7 @@ object BuildHelper {
   def stdSettings(prjName: String) = Seq(
     name                                   := s"$prjName",
     crossScalaVersions                     := Seq(Scala212, Scala213),
-    ThisBuild / scalaVersion               := Scala213,
+    ThisBuild / scalaVersion               := ScalaDotty,
     scalacOptions                          := stdOptions ++ extraOptions(scalaVersion.value, optimize = !isSnapshot.value),
     libraryDependencies ++= {
       if (scalaVersion.value == ScalaDotty)

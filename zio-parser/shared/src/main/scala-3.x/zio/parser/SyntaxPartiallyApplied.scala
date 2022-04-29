9f3ef9311e0eb76ef328f1e0653cb26ca913fefd
NavidKJalali <navid.jalali@prezi.com>
2022-04-29 16:16:14 +0200

    🔧 remove testing leftovers

--- zio-parser/shared/src/main/scala-3.x/zio/parser/SyntaxPartiallyApplied.scala
+++
@@ -1,9 +0,0 @@
-package zio.parser
-
-final class SyntaxPartiallyApplied[A] {
-  inline def apply[I, O](
-      inline syntax: Syntax[String, I, O, _ <: A],
-      inline syntaxes: Syntax[String, I, O, _ <: A]*
-  ): Syntax[String, I, O, A] =
-    ${ Macros.oneOfImpl[A, I, O]('syntax, 'syntaxes) }
-}

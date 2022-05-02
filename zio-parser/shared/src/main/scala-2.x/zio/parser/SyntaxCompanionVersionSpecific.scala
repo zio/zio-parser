b296724f9d809431df4cb6581eb1c5758a2d013e
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2022-05-02 22:44:45 +0100

    Format

--- zio-parser/shared/src/main/scala-2.x/zio/parser/SyntaxCompanionVersionSpecific.scala
+++ zio-parser/shared/src/main/scala-2.x/zio/parser/SyntaxCompanionVersionSpecific.scala
@@ -2,8 +2,8 @@ package zio.parser
 
 trait SyntaxCompanionVersionSpecific {
   def oneOf[A, I, O](
-    syntax: Syntax[String, I, O, _ <: A],
-    syntaxes: Syntax[String, I, O, _ <: A]*,
-  ): Syntax[String, I, O, A] = 
+      syntax: Syntax[String, I, O, _ <: A],
+      syntaxes: Syntax[String, I, O, _ <: A]*
+  ): Syntax[String, I, O, A] =
     syntaxes.map(_.widen[A]).foldLeft(syntax.widen[A])(_ | _)
 }

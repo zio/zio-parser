7bdf551f252cb84416206ae25c9c44750c596667
Milad Khajavi <khajavi@gmail.com>
2023-03-08 22:35:01 +0330

    fmt.

--- zio-parser/shared/src/main/scala-3.x/zio/parser/TupleConversion.scala
+++ zio-parser/shared/src/main/scala-3.x/zio/parser/TupleConversion.scala
@@ -10,18 +10,22 @@ trait TupleConversion[A, B] {
 object TupleConversion extends ImplicitTupleConversion
 
 trait ImplicitTupleConversion {
-  inline given autoTupleConversion[Prod <: Product](using m: Mirror.ProductOf[Prod]): TupleConversion[Prod, m.MirroredElemTypes] =
-  new TupleConversion[Prod, m.MirroredElemTypes] {
-    def to(a: Prod): m.MirroredElemTypes = Tuple.fromProductTyped(a)
-    def from(b: m.MirroredElemTypes): Prod = m.fromProduct(b)
-  }
-
-  inline given autoTupleConversion1[Prod <: Product, A](using c: TupleConversion[Prod, Tuple1[A]]): TupleConversion[Prod, A] =
-  new TupleConversion[Prod, A] {
-    def to(a: Prod): A = {
-      val Tuple1(v) = c.to(a)
-      v
+  inline given autoTupleConversion[Prod <: Product](using
+      m: Mirror.ProductOf[Prod]
+  ): TupleConversion[Prod, m.MirroredElemTypes] =
+    new TupleConversion[Prod, m.MirroredElemTypes] {
+      def to(a: Prod): m.MirroredElemTypes   = Tuple.fromProductTyped(a)
+      def from(b: m.MirroredElemTypes): Prod = m.fromProduct(b)
     }
-    def from(b: A): Prod = c.from(Tuple1(b))
-  }
+
+  inline given autoTupleConversion1[Prod <: Product, A](using
+      c: TupleConversion[Prod, Tuple1[A]]
+  ): TupleConversion[Prod, A] =
+    new TupleConversion[Prod, A] {
+      def to(a: Prod): A   = {
+        val Tuple1(v) = c.to(a)
+        v
+      }
+      def from(b: A): Prod = c.from(Tuple1(b))
+    }
 }
